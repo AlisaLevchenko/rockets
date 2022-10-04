be5fa7d5685c33fd9984cffd7f5bf728e946ff39
AlisaLevchenko <103849753+AlisaLevchenko@users.noreply.github.com>
2022-10-04 18:47:16 +0300

    add image

--- src/components/rocketsList/RocketsList.jsx
+++ src/components/rocketsList/RocketsList.jsx
@@ -16,6 +16,7 @@ export default function RocketsList({ dragons }) {
               />
               {name}
             </NavLink>
+            <img style={{ width: "250px" }} src={placeholder} alt={`Rocket`} />
           </li>
         ))}
       </ul>
